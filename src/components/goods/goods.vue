b23ff3c96c708b700cf03cd019017e15bcd9b351
CodingEye <Huai0125@163.com>
2017-03-31 23:03:40 +0800

    Add menu-wrapper and foods-wrapper

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,170 @@
 <template>
-  <div>I am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper" v-el:menu-wrapper>
+      <ul>
+        <li v-for="item in goods" class="menu-item-wrapper">
+          <span class="menu-item border-1px">
+            <span v-show="item.type > 0" class="icon" :class="classMap[item.type]">
+            </span>
+            <span class="text">{{item.name}}</span>
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper" v-el:foods-wrapper>
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item border-1px">
+              <div class="icon">
+                <img width="57" height="57" :src="food.icon">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="current">¥{{food.price}}</span>
+                  <span class="past" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-   export default{};
+  import BScroll from 'better-scroll';
+  const NO_ERR = 0;
+
+  export default{
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        goods: []
+      };
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+      this.$http.get('/api/goods').then((response) => {
+        response = response.body;
+        if (response.errno === NO_ERR) {
+          this.goods = response.data;
+          this.$nextTick(() => {
+            this._initScroll();
+          });
+        }
+      });
+    },
+    methods: {
+        _initScroll() {
+            this.menuScroll = new BScroll(this.$els.menuWrapper, {});
+            this.menuScroll = new BScroll(this.$els.foodsWrapper, {});
+        }
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
 
+  .goods
+    display flex
+    position absolute
+    top 174px
+    bottom 46px
+    width 100%
+    overflow hidden
+    .menu-wrapper
+      flex 0 0 80px
+      width 80px
+      background #f3f5f7
+      .menu-item-wrapper
+        display table
+        height 54px
+        width 56px
+        line-height 14px
+        padding 0 12px
+        .menu-item
+          display table-cell
+          width 56px
+          vertical-align middle
+          border-1px(rgba(7, 17, 27, 0.1))
+          .icon
+            display inline-block
+            width 12px
+            height 12px
+            background-size 12px 12px
+            background-repeat no-repeat
+            &.decrease
+              bg-image('decrease_3')
+            &.discount
+              bg-image('discount_3')
+            &.guarantee
+              bg-image('guarantee_3')
+            &.invoice
+              bg-image('invoice_3')
+            &.special
+              bg-image('special_3')
+          .text
+            vertical-align top
+            font-size 12px
+    .foods-wrapper
+      flex 1
+      .title
+        padding-left 14px
+        height 26px
+        line-height 26px
+        border-left 2px solid #d9dde1
+        font-size 12px
+        color rgb(147, 153, 159)
+        background #f3f5f7
+      .food-item
+        display flex
+        margin 18px 18px 0 18px
+        padding-bottom 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        &:last-child
+          border-none()
+        .icon
+          flex 0 0 57px
+          margin-right 10px
+        .content
+          flex 1
+          .name
+            margin 2px 0 8px 0
+            height 14px
+            line-height 14px
+            color rgb(7, 17, 27)
+            font-size 14px
+          .desc, .extra
+            font-size 10px
+            color rgb(147, 153, 159)
+            line-height 12px
+          .desc
+            margin-bottom 8px
+          .extra
+            .count
+              margin-right 12px
+          .price
+            font-weight 700
+            line-height 24px
+            .current
+              margin-right 8px
+              color rgb(240, 20, 20)
+              font-size 14px
+            .past
+              text-decoration line-through
+              font-size 10px
+              color rgb(147, 153, 159)
 </style>
