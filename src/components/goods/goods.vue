c556f83a757df5c5e4bc7d8fbe2ff76b43cdc1f0
CodingEye <Huai0125@163.com>
2017-04-01 23:01:12 +0800

    Add scroll part

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,8 @@
   <div class="goods">
     <div class="menu-wrapper" v-el:menu-wrapper>
       <ul>
-        <li v-for="item in goods" class="menu-item-wrapper">
+        <li v-for="item in goods" class="menu-item-wrapper" :class="{'current':currentIndex===$index}"
+            @click="selectMenu($index,$event)">
           <span class="menu-item border-1px">
             <span v-show="item.type > 0" class="icon" :class="classMap[item.type]">
             </span>
@@ -13,7 +14,7 @@
     </div>
     <div class="foods-wrapper" v-el:foods-wrapper>
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px">
@@ -51,8 +52,25 @@
     },
     data() {
       return {
-        goods: []
+        goods: [],
+        listHeight: [],
+        scrollY: 0
       };
+    },
+    computed: {
+      currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          if (i === this.listHeight.length - 1) {
+            return i;
+          }
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if (this.scrollY >= height1 && this.scrollY < height2) {
+            return i;
+          }
+        }
+        return 0;
+      }
     },
     created() {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
@@ -62,15 +80,41 @@
           this.goods = response.data;
           this.$nextTick(() => {
             this._initScroll();
+            this._calculateHeight();
           });
         }
       });
     },
     methods: {
-        _initScroll() {
-            this.menuScroll = new BScroll(this.$els.menuWrapper, {});
-            this.menuScroll = new BScroll(this.$els.foodsWrapper, {});
+      _initScroll() {
+        this.menuScroll = new BScroll(this.$els.menuWrapper, {
+          click: true
+        });
+        this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
+          probeType: 3
+        });
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        });
+      },
+      _calculateHeight() {
+        let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
         }
+      },
+      selectMenu(index, event) {
+        if (!event._constructed) {
+          return;
+        }
+        let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+        let el = foodList[index];
+        this.foodsScroll.scrollToElement(el, 300);
+      }
     }
   };
 </script>
@@ -95,6 +139,14 @@
         width 56px
         line-height 14px
         padding 0 12px
+        &.current
+          position relative
+          z-index 10
+          margin-top -1px
+          background #fff
+          font-weight 700
+          .text
+            border-none()
         .menu-item
           display table-cell
           width 56px
--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>I am seller</div>
+  <div class="pending">
+    <div class="text">商家模块正在施工中...</div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -7,5 +9,13 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .pending
+    position absolute
+    width 100%
+    height 100%
+    display table
+    .text
+      display table-cell
+      text-align center
+      padding-top 40%
 </style>
